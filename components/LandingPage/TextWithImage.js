528c3f04b5900baf1a096a714485cd0357732889
M Afzaal Afzal <afzaal1721@gmail.com>
2021-03-29 01:48:41 +0500

    Secret chat implemented.

--- components/LandingPage/TextWithImage.js
+++ components/LandingPage/TextWithImage.js
@@ -83,6 +83,20 @@ const useStyles = makeStyles(theme => ({
     },
 }))
 
+//<!--Start of Tawk.to Script-->
+// <script type="text/javascript">
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/6060d0f8f7ce18270934c991/1f1t30jsk';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
+// </script>
+// <!--End of Tawk.to Script-->
+
 const TextWithImage = (props) => {
 
     const classes = useStyles();
--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, {useEffect} from 'react';
 import Header from "./Header";
-import {Box, IconButton, makeStyles} from "@material-ui/core";
+import { makeStyles} from "@material-ui/core";
 import Footer from "./Footer";
-import Image from "next/image";
-import ChatRoom from "../Chat/ChatRoom";
+// import Image from "next/image";
+// import ChatRoom from "../Chat/ChatRoom";
 
 const useStyles = makeStyles(theme => ({
     toolbarMargin: {
@@ -28,11 +28,25 @@ const Layout = (props) => {
 
     const classes = useStyles();
 
-    const [isChatOpened, setIsChatOpened] = useState(false);
+    // const [isChatOpened, setIsChatOpened] = useState(false);
 
-    const toggleIsChatOpened = () => {
-        setIsChatOpened(!isChatOpened);
-    }
+    // const toggleIsChatOpened = () => {
+    //     setIsChatOpened(!isChatOpened);
+    // }
+
+    useEffect(() => {
+    const Tawk_API = '75073c4b3184272df207bc7eefc1f91c42d600d3' || {}, Tawk_LoadStart = new Date();
+    (function(){
+    const s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/6060d0f8f7ce18270934c991/1f1t30jsk';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+
+    },[])
+
 
     return (
         <>
@@ -42,13 +56,14 @@ const Layout = (props) => {
 
             {props.children}
 
-            <Box className={classes.messageIcon}>
-                <IconButton color={'primary'} onClick={toggleIsChatOpened}>
-                    <Image priority alt={'Chat Icon'} style={{zIndex: 50}} src={'/chatbubbles-icon.svg'} width={30} height={30}/>
-                </IconButton>
-            </Box>
+            {/*<Box className={classes.messageIcon}>*/}
+            {/*    <IconButton color={'primary'} onClick={toggleIsChatOpened}>*/}
+            {/*        <Image priority alt={'Chat Icon'} style={{zIndex: 50}} src={'/chatbubbles-icon.svg'} width={30}*/}
+            {/*               height={30}/>*/}
+            {/*    </IconButton>*/}
+            {/*</Box>*/}
 
-            <ChatRoom isChatOpened={isChatOpened} toggleIsChatOpened={toggleIsChatOpened}/>
+            {/*<ChatRoom isChatOpened={isChatOpened} toggleIsChatOpened={toggleIsChatOpened}/>*/}
 
             <Footer/>
         </>
